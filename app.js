f38917ede99451257c1351bce2ff7a4c2e9953d0
Rayan Zhang <844346942@qq.com>
2020-01-07 16:12:46 +0800

    0.2.0 minor fix
    1. # 后的参数直接去除；
    2. 将脚本执行的时机提前；
    3. 更新 README，添加更新内容历史。

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 // ==UserScript==
 // @name         获取中文版页面 Jump To Chinese Page
 // @namespace    https://github.com/moonflame/jump-to-chinese-page
-// @version      0.1.1
+// @version      0.2.0
 // @description  检测当前浏览的 MDN 非中文页面，自动跳转到对应中文版页面。适用于 Tampermonkey Chrome 扩展
 // @author       Rayan Zhang
 // @match        *://developer.mozilla.org/*
 // @grant        none
+// @run-at       document-start // The script will be injected as fast as possible.
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let originalUrl = window.location.href;
-    let dividedUrlArr = originalUrl.split('/');
+    let urlOriginal = window.location.href;
+    let urlArrDivided = urlOriginal.split('/');
+    let urlProcessing;
 
-    if (dividedUrlArr[2] == 'developer.mozilla.org') {
-        if (dividedUrlArr[3] == 'zh-CN') {
+    if (urlArrDivided[2] == 'developer.mozilla.org') { // MDN
+        if (urlArrDivided[3] == 'zh-CN') {
             return;
         } else {
-            dividedUrlArr[3] = 'zh-CN';
-            window.location.href = dividedUrlArr.join('/');
+            urlArrDivided[3] = 'zh-CN';
+            urlProcessing = urlArrDivided.join('/'); // developer.mozilla.org/zh-CN/...
+            urlArrDivided = urlProcessing.split('#'); // after language change, parameters after "#" is useless
+            window.location.href = urlArrDivided[0]; // load new page
             return;
         }
     }

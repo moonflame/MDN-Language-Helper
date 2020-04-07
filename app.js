c29267ff656fd41154a2901665020e0d1ea7b328
Rayan Zhang <844346942@qq.com>
2020-04-08 00:41:39 +0800

    0.3.0 项目更名为 MDN Language Helper，添加页面菜单。
    1. 项目更名为 MDN Language Helper；
    2. 脚本注入时机延后以添加 DOM 元素；
    3. 取消去除 # 后的参数；

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         获取中文版页面 Jump To Chinese Page
+// @name         MDN 语言助手 MDN Language Helper
 // @namespace    https://github.com/moonflame/jump-to-chinese-page
-// @version      0.2.0
+// @version      0.3.0
 // @description  检测当前浏览的 MDN 非中文页面，自动跳转到对应中文版页面。适用于 Tampermonkey Chrome 扩展
 // @author       Rayan Zhang
 // @match        *://developer.mozilla.org/*
 // @grant        none
-// @run-at       document-start // The script will be injected as fast as possible.
+// @run-at       document-end // The script will be injected when or after the DOMContentLoaded event was dispatched.
 // ==/UserScript==
 
 (function() {
@@ -18,13 +18,36 @@
 
     if (urlArrDivided[2] == 'developer.mozilla.org') { // MDN
         if (urlArrDivided[3] == 'zh-CN') {
-            return;
+            // return;
         } else {
             urlArrDivided[3] = 'zh-CN';
             urlProcessing = urlArrDivided.join('/'); // developer.mozilla.org/zh-CN/...
-            urlArrDivided = urlProcessing.split('#'); // after language change, parameters after "#" is useless
-            window.location.href = urlArrDivided[0]; // load new page
-            return;
+            // urlArrDivided = urlProcessing.split('#'); // after language change, parameters after "#" is useless
+            window.location.href = urlProcessing; // load new page
+            // return;
         }
     }
+
+    // append a list named "Language Helper"
+    let el = document.createElement("li");
+    el.setAttribute("class", "top-level-entry-container");
+
+    let tempEl = document.createElement("button");
+    tempEl.setAttribute("type", "button");
+    tempEl.setAttribute("class", "top-level-entry");
+    tempEl.setAttribute("aria-haspopup", "true");
+
+    let tempTempEl = document.createTextNode("Language Helper");
+    tempEl.appendChild(tempTempEl);
+
+    tempTempEl = document.createElement("span");
+    tempTempEl.setAttribute("class", "main-menu-arrow");
+    tempTempEl.setAttribute("aria-hidden", "true");
+    tempTempEl.appendChild(document.createTextNode("▼"));
+    tempEl.appendChild(tempTempEl);
+
+    el.appendChild(tempEl);
+
+    let currentDiv = document.getElementsByClassName("main-nav");
+    currentDiv[0].children[0].appendChild(el, currentDiv[0]);
 })();

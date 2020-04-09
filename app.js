fe4147350c322c089c9648f37a17932f5fecf45c
Rayan Zhang <844346942@qq.com>
2020-04-09 22:01:28 +0800

    0.3.1 添加下拉菜单项目。

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         MDN 语言助手 MDN Language Helper
 // @namespace    https://github.com/moonflame/jump-to-chinese-page
-// @version      0.3.0
+// @version      0.3.1
 // @description  检测当前浏览的 MDN 非中文页面，自动跳转到对应中文版页面。适用于 Tampermonkey Chrome 扩展
 // @author       Rayan Zhang
 // @match        *://developer.mozilla.org/*
 // @grant        none
-// @run-at       document-end // The script will be injected when or after the DOMContentLoaded event was dispatched.
+// @run-at       document-start // The script will be injected as fast as possible.
 // ==/UserScript==
 
 (function() {
     'use strict';
+    let scriptName = "Language Helper";
 
     let urlOriginal = window.location.href;
     let urlArrDivided = urlOriginal.split('/');
@@ -30,23 +31,38 @@
 
     // append a list named "Language Helper"
     let el = document.createElement("li");
+    let temp1, temp2, temp3, temp4;
     el.setAttribute("class", "top-level-entry-container");
 
-    let tempEl = document.createElement("button");
-    tempEl.setAttribute("type", "button");
-    tempEl.setAttribute("class", "top-level-entry");
-    tempEl.setAttribute("aria-haspopup", "true");
+    temp1 = document.createElement("button");
+    temp1.setAttribute("type", "button");
+    temp1.setAttribute("class", "top-level-entry");
+    temp1.setAttribute("aria-haspopup", "true");
 
-    let tempTempEl = document.createTextNode("Language Helper");
-    tempEl.appendChild(tempTempEl);
+    temp2 = document.createTextNode(scriptName);
+    temp1.appendChild(temp2);
 
-    tempTempEl = document.createElement("span");
-    tempTempEl.setAttribute("class", "main-menu-arrow");
-    tempTempEl.setAttribute("aria-hidden", "true");
-    tempTempEl.appendChild(document.createTextNode("▼"));
-    tempEl.appendChild(tempTempEl);
+    temp2 = document.createElement("span");
+    temp2.setAttribute("class", "main-menu-arrow");
+    temp2.setAttribute("aria-hidden", "true");
+    temp2.appendChild(document.createTextNode("▼"));
+    temp1.appendChild(temp2);
 
-    el.appendChild(tempEl);
+    el.appendChild(temp1);
+
+    temp1 = document.createElement("ul");
+    temp2 = document.createElement("li");
+    temp2.setAttribute("data-item", scriptName);
+    temp2.setAttribute("role", "menuitem");
+
+    temp3 = document.createElement("a");
+    temp3.setAttribute("href", "Javascript:void(0)");
+    temp4 = document.createTextNode("On/Off On");
+
+    el.appendChild(temp1);
+    temp1.appendChild(temp2);
+    temp2.appendChild(temp3);
+    temp3.appendChild(temp4);
 
     let currentDiv = document.getElementsByClassName("main-nav");
     currentDiv[0].children[0].appendChild(el, currentDiv[0]);
